de1c4fdd3e86de1b4c85fb43a7f100490460fb7f
michaelhly <michaelhly@gmail.com>
2020-08-21 14:38:05 -0500

    Test for transaction.populate

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,18 +7,18 @@ from solanaweb3.publickey import PublicKey
 
 
 @pytest.fixture(scope="session")
-def test_blockhash() -> Blockhash:
+def stubbed_blockhash() -> Blockhash:
     """Arbitrary block hash."""
     return Blockhash("EETubP5AKHgjPAhzPAFcb8BAY1hMH639CWCFTqi3hq1k")
 
 
 @pytest.fixture(scope="session")
-def test_reciever() -> PublicKey:
+def stubbed_reciever() -> PublicKey:
     """Arbitrary known public key to be used as reciever."""
     return PublicKey("J3dxNj7nDRRqRRXuEMynDG57DkZK4jYRuv3Garmb1i99")
 
 
 @pytest.fixture(scope="session")
-def test_sender() -> Account:
+def stubbed_sender() -> Account:
     """Arbitrary known account to be used as sender."""
     return Account(bytes([8] * PublicKey.LENGTH))
--- tests/solanaweb3/test_transaction.py
+++ tests/solanaweb3/test_transaction.py
@@ -1,7 +1,26 @@
 """Unit tests for solanaweb3.transaction"""
+from base58 import b58encode
+
+import solanaweb3.transaction as txlib
+from solanaweb3.message import CompiledInstruction, Message, MessageArgs, MessageHeader
+from solanaweb3.publickey import PublicKey
 
 
-def test_serialize_unsigned_transaction(test_blockhash, test_reciever, test_sender):
-    """Test to serialize an unsigned transaction."""
-    # TODO(michael): Write this test
-    assert True
+def test_populate(stubbed_blockhash):
+    """Test populating transaction with a message and two signatures."""
+    account_keys = [str(PublicKey(i + 1)) for i in range(5)]
+    msg = Message(
+        MessageArgs(
+            account_keys=account_keys,
+            header=MessageHeader(
+                num_readonly_signed_accounts=0, num_readonly_unsigned_accounts=3, num_required_signatures=2
+            ),
+            instructions=[CompiledInstruction(accounts=[1, 2, 3], data=b58encode(bytes([9] * 5)), program_id_index=4)],
+            recent_blockhash=stubbed_blockhash,
+        )
+    )
+    signatures = [b58encode(bytes([1] * txlib.SIG_LENGTH)), b58encode(bytes([2] * txlib.SIG_LENGTH))]
+    transaction = txlib.Transaction.populate(msg, signatures)
+    assert len(transaction.instructions) == len(msg.instructions)
+    assert len(transaction.signatures) == len(signatures)
+    assert transaction.recent_blockhash == msg.recent_blockhash
