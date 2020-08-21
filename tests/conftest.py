ff63f89183d502b405342de078c35cb6654c4942
michaelhly <michaelhly@gmail.com>
2020-08-21 03:29:46 -0500

    Fixup

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,17 +8,17 @@ from solanaweb3.publickey import PublicKey
 
 @pytest.fixture(scope="session")
 def test_blockhash() -> Blockhash:
-    """Arbitrary block hash"""
+    """Arbitrary block hash."""
     return Blockhash("EETubP5AKHgjPAhzPAFcb8BAY1hMH639CWCFTqi3hq1k")
 
 
 @pytest.fixture(scope="session")
-def test_reciever() -> "PublicKey":
+def test_reciever() -> PublicKey:
     """Arbitrary known public key to be used as reciever."""
     return PublicKey("J3dxNj7nDRRqRRXuEMynDG57DkZK4jYRuv3Garmb1i99")
 
 
 @pytest.fixture(scope="session")
-def test_sender() -> "Account":
+def test_sender() -> Account:
     """Arbitrary known account to be used as sender."""
     return Account(bytes([8] * 32))
