940aa115642403f8418294358131c6b7f6d29e03
michaelhly <michaelhly@gmail.com>
2020-08-26 02:09:28 -0500

    Update conf.py

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -30,7 +30,7 @@ author = "Michael Huang"
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx.ext.viewcode", "sphinx.ext.coverage", "sphinx.ext.doctest"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+# templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -48,4 +48,4 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
