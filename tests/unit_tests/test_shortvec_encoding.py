c72d19a367f3dab45302f2bd023ed85c7417990c
michaelhly <michaelhly@gmail.com>
2020-08-27 02:37:49 -0500

    Remove unnecessary return

--- tests/unit_tests/test_shortvec_encoding.py
+++ tests/unit_tests/test_shortvec_encoding.py
@@ -24,13 +24,13 @@ def test_decode_length():
     assert_decoded_array(bytes([0x80, 0x80, 0x80, 0x01]), 4, 0x200000)
 
 
-def assert_encoded_array(buffer: bytearray, length: int, prev_length: int, expected: List[int]) -> bytearray:
+def assert_encoded_array(buffer: bytearray, length: int, prev_length: int, expected: List[int]) -> None:
+    """Helper to encode length of an array."""
     assert len(buffer) == prev_length
     actual = encode_length(length)
     buffer.extend(actual)
     assert len(buffer) == prev_length + len(expected)
     assert bytes(buffer[-len(expected) :]) == bytes(expected)  # noqa: 203
-    return buffer
 
 
 def test_encode_length():
@@ -39,31 +39,31 @@ def test_encode_length():
     prev_length = 0
 
     expected = [0]
-    buffer = assert_encoded_array(buffer, 0, prev_length, expected)
+    assert_encoded_array(buffer, 0, prev_length, expected)
     prev_length += len(expected)
 
     expected = [5]
-    buffer = assert_encoded_array(buffer, 5, prev_length, expected)
+    assert_encoded_array(buffer, 5, prev_length, expected)
     prev_length += len(expected)
 
     expected = [0x7F]
-    buffer = assert_encoded_array(buffer, 0x7F, prev_length, expected)
+    assert_encoded_array(buffer, 0x7F, prev_length, expected)
     prev_length += len(expected)
 
     expected = [0x80, 0x01]
-    buffer = assert_encoded_array(buffer, 0x80, prev_length, expected)
+    assert_encoded_array(buffer, 0x80, prev_length, expected)
     prev_length += len(expected)
 
     expected = [0xFF, 0x01]
-    buffer = assert_encoded_array(buffer, 0xFF, prev_length, expected)
+    assert_encoded_array(buffer, 0xFF, prev_length, expected)
     prev_length += len(expected)
 
     expected = [0x80, 0x02]
-    buffer = assert_encoded_array(buffer, 0x100, prev_length, expected)
+    assert_encoded_array(buffer, 0x100, prev_length, expected)
     prev_length += len(expected)
 
     expected = [0xFF, 0xFF, 0x01]
-    buffer = assert_encoded_array(buffer, 0x7FFF, prev_length, expected)
+    assert_encoded_array(buffer, 0x7FFF, prev_length, expected)
     prev_length += len(expected)
 
     expected = [
@@ -72,7 +72,7 @@ def test_encode_length():
         0x80,
         0x01,
     ]
-    buffer = assert_encoded_array(buffer, 0x200000, prev_length, expected)
+    assert_encoded_array(buffer, 0x200000, prev_length, expected)
     prev_length += len(expected)
 
     assert prev_length == len(buffer) == 16
