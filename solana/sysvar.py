0b8a0fdaedd917b83ac4b5e2c62b0b7848a4a19f
michaelhly <michaelhly@gmail.com>
2020-08-29 22:37:52 -0500

    Fixup

--- solana/sysvar.py
+++ solana/sysvar.py
@@ -1,4 +1,4 @@
-"""Solana sysvars that allow programs to access certain network state."""
+"""Syntethic accounts that allow programs to access certain network states."""
 from solana.publickey import PublicKey
 
 SYSVAR_CLOCK_PUBKEY: PublicKey = PublicKey("SysvarC1ock11111111111111111111111111111111")
