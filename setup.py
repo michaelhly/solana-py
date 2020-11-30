97a62c60b829ba4d3ebcc8f2c3473d19d8dd4aea
michaelhly <michaelhly@gmail.com>
2020-11-29 23:30:19 -0600

    Bump version: 0.6.4 → 0.6.5

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("README.md", "r") as file_handle:
 setup(
     name="solana",
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version="0.6.4",
+    version="0.6.5",
     author="Michael Huang",
     author_mail="michaelhly@gmail.com",
     description="""Solana.py""",
