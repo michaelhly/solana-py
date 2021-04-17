615064316d0ad3d5d0594189821f69ef8a261180
michaelhly <michaelhly@gmail.com>
2021-04-17 18:20:36 -0500

    Add python3.9 to classifiers

--- setup.py
+++ setup.py
@@ -54,5 +54,6 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
