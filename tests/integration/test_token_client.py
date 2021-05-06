79582eb01472f3ba8c027afff0d3143f68f79a1f
Dave <DaveWK@users.noreply.github.com>
2021-05-06 15:30:03 -0400

    Make new account for ATA test

--- tests/integration/test_token_client.py
+++ tests/integration/test_token_client.py
@@ -87,7 +87,7 @@ def test_new_account(stubbed_sender, test_http_client, test_token):  # pylint: d
 @pytest.mark.integration
 def test_new_associated_account(test_token):  # pylint: disable=redefined-outer-name
     """Test creating a new associated token account."""
-    new_acct = PublicKey(0)
+    new_acct = Account().public_key()
     token_account_pubkey = test_token.create_associated_token_account(new_acct)
     expected_token_account_key, _ = new_acct.find_program_address(
         seeds=[bytes(new_acct), bytes(TOKEN_PROGRAM_ID), bytes(test_token.pubkey)],
