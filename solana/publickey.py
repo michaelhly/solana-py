756e5651411f70c68e396f12a1aa585cade521b2
Dave <DaveWK@users.noreply.github.com>
2021-05-06 13:11:39 -0400

    nonce = 254

--- solana/publickey.py
+++ solana/publickey.py
@@ -86,7 +86,7 @@ class PublicKey:
         iterates a nonce until it finds one that when combined with the seeds
         results in a valid program address.
         """
-        nonce = 255
+        nonce = 254
         while nonce != 0:
             try:
                 buffer = seeds + [helpers.to_uint8_bytes(nonce)]
