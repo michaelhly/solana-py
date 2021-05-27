9d4d9c3a225e838202071259194c69e2b8561004
Michael Huang <michaelhly@gmail.com>
2021-05-26 20:55:33 -0500

    Update solana/__init__.py

--- solana/__init__.py
+++ solana/__init__.py
@@ -1,4 +1,4 @@
-"""Solana.py is the Python API for interfacing with the `Solana JSON RPC <https://docs.solana.com/developing/clients/jsonrpc-api/>`_. # noqa: D400, D415, E501 # pylint: disable=line-too-long
+"""Solana.py is the Python API for interfacing with the `Solana JSON RPC <https://docs.solana.com/developing/clients/jsonrpc-api/>`_. # noqa: E501 # pylint: disable=line-too-long
 
 Installation
 ------------
