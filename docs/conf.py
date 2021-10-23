3e175f4082925a1f44e0761a9f24140b3c36c766
kevinheavey <kevinheavey123@gmail.com>
2021-10-23 16:15:41 +0100

    remove unused import

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
 from pathlib import Path
 import sys
 
