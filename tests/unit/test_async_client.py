d7b62f589d7561b79ff08550cc8086bfa80b4651
broper2 <brianroper7@gmail.com>
2021-12-22 16:05:41 -0600

    lint cleanup

--- tests/unit/test_async_client.py
+++ tests/unit/test_async_client.py
@@ -24,32 +24,44 @@ async def test_async_client_http_exception(unit_test_http_client_async):
 
 
 def test_client_address2_sig_args_no_commitmment(unit_test_http_client_async):
-    expected = ("getConfirmedSignaturesForAddress2", "11111111111111111111111111111111", {
-        "before": "before", "until": "until", "limit": 5
-    })
-    actual = unit_test_http_client_async._get_confirmed_signature_for_address2_args(PublicKey(0), "before", "until", 5, None)
+    expected = (
+        "getConfirmedSignaturesForAddress2",
+        "11111111111111111111111111111111",
+        {"before": "before", "until": "until", "limit": 5},
+    )
+    actual = unit_test_http_client_async._get_confirmed_signature_for_address2_args(
+        PublicKey(0), "before", "until", 5, None
+    )
     assert expected == actual
 
 
 def test_client_address_sig_args_no_commitment(unit_test_http_client_async):
-    expected = ("getSignaturesForAddress", "11111111111111111111111111111111", {
-        "before": "before", "until": "until", "limit": 5
-    })
+    expected = (
+        "getSignaturesForAddress",
+        "11111111111111111111111111111111",
+        {"before": "before", "until": "until", "limit": 5},
+    )
     actual = unit_test_http_client_async._get_signatures_for_address_args(PublicKey(0), "before", "until", 5, None)
     assert expected == actual
 
 
-def test_client_address_sig_args_with_commitment(unit_test_http_client_async):
-    expected = ("getSignaturesForAddress", "11111111111111111111111111111111", {
-        "limit": 5, "commitment": "finalized"
-    })
-    actual = unit_test_http_client_async._get_signatures_for_address_args(PublicKey(0), None, None, 5, Finalized)
+def test_client_address2_sig_args_with_commitmment(unit_test_http_client_async):
+    expected = (
+        "getConfirmedSignaturesForAddress2",
+        "11111111111111111111111111111111",
+        {"limit": 5, "commitment": "finalized"},
+    )
+    actual = unit_test_http_client_async._get_confirmed_signature_for_address2_args(
+        PublicKey(0), None, None, 5, Finalized
+    )
     assert expected == actual
 
 
-def test_client_address2_sig_args_with_commitmment(unit_test_http_client_async):
-    expected = ("getConfirmedSignaturesForAddress2", "11111111111111111111111111111111", {
-        "limit": 5, "commitment": "finalized"
-    })
-    actual = unit_test_http_client_async._get_confirmed_signature_for_address2_args(PublicKey(0), None, None, 5, Finalized)
+def test_client_address_sig_args_with_commitment(unit_test_http_client_async):
+    expected = (
+        "getSignaturesForAddress",
+        "11111111111111111111111111111111",
+        {"limit": 5, "commitment": "finalized"},
+    )
+    actual = unit_test_http_client_async._get_signatures_for_address_args(PublicKey(0), None, None, 5, Finalized)
     assert expected == actual
--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -22,32 +22,40 @@ def test_client_http_exception(unit_test_http_client):
 
 
 def test_client_address2_sig_args_no_commitmment(unit_test_http_client):
-    expected = ("getConfirmedSignaturesForAddress2", "11111111111111111111111111111111", {
-        "before": "before", "until": "until", "limit": 5
-    })
+    expected = (
+        "getConfirmedSignaturesForAddress2",
+        "11111111111111111111111111111111",
+        {"before": "before", "until": "until", "limit": 5},
+    )
     actual = unit_test_http_client._get_confirmed_signature_for_address2_args(PublicKey(0), "before", "until", 5, None)
     assert expected == actual
 
 
 def test_client_address_sig_args_no_commitment(unit_test_http_client):
-    expected = ("getSignaturesForAddress", "11111111111111111111111111111111", {
-        "before": "before", "until": "until", "limit": 5
-    })
+    expected = (
+        "getSignaturesForAddress",
+        "11111111111111111111111111111111",
+        {"before": "before", "until": "until", "limit": 5},
+    )
     actual = unit_test_http_client._get_signatures_for_address_args(PublicKey(0), "before", "until", 5, None)
     assert expected == actual
 
 
-def test_client_address_sig_args_with_commitment(unit_test_http_client):
-    expected = ("getSignaturesForAddress", "11111111111111111111111111111111", {
-        "limit": 5, "commitment": "finalized"
-    })
-    actual = unit_test_http_client._get_signatures_for_address_args(PublicKey(0), None, None, 5, Finalized)
+def test_client_address2_sig_args_with_commitmment(unit_test_http_client):
+    expected = (
+        "getConfirmedSignaturesForAddress2",
+        "11111111111111111111111111111111",
+        {"limit": 5, "commitment": "finalized"},
+    )
+    actual = unit_test_http_client._get_confirmed_signature_for_address2_args(PublicKey(0), None, None, 5, Finalized)
     assert expected == actual
 
 
-def test_client_address2_sig_args_with_commitmment(unit_test_http_client):
-    expected = ("getConfirmedSignaturesForAddress2", "11111111111111111111111111111111", {
-        "limit": 5, "commitment": "finalized"
-    })
-    actual = unit_test_http_client._get_confirmed_signature_for_address2_args(PublicKey(0), None, None, 5, Finalized)
+def test_client_address_sig_args_with_commitment(unit_test_http_client):
+    expected = (
+        "getSignaturesForAddress",
+        "11111111111111111111111111111111",
+        {"limit": 5, "commitment": "finalized"},
+    )
+    actual = unit_test_http_client._get_signatures_for_address_args(PublicKey(0), None, None, 5, Finalized)
     assert expected == actual
