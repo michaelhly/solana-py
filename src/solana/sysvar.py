eb03b092a9a013578caab2e7a07ebcb5e44d84d1
Michael Huang <michaelhly@gmail.com>
2021-12-24 11:43:55 -0600

    Add more sysvar constants (#159)
    * Add more sysvar constants
    
    * Update docs
    
    * lint

--- src/solana/sysvar.py
+++ src/solana/sysvar.py
@@ -15,3 +15,20 @@ SYSVAR_REWARDS_PUBKEY: PublicKey = PublicKey("SysvarRewards111111111111111111111
 
 SYSVAR_STAKE_HISTORY_PUBKEY: PublicKey = PublicKey("SysvarStakeHistory1111111111111111111111111")
 """Public key of the synthetic account that serves the stake history."""
+
+SYSVAR_EPOCH_SCHEDULE_PUBKEY: PublicKey = PublicKey("SysvarEpochSchedu1e111111111111111111111111")
+"""The EpochSchedule sysvar contains epoch scheduling constants that are set in genesis, and enables calculating the
+number of slots in a given epoch, the epoch for a given slot, etc. (Note: the epoch schedule is distinct from the
+`leader schedule <https://docs.solana.com/terminology#leader-schedule>`_).
+"""
+
+SYSVAR_INSTRUCTIONS_PUBKEY: PublicKey = PublicKey("Sysvar1nstructions1111111111111111111111111")
+"""
+The Instructions sysvar contains the serialized instructions in a Message while that Message is being processed.
+This allows program instructions to reference other instructions in the same transaction.
+Read more information on `instruction introspection
+<https://docs.solana.com/implemented-proposals/instruction_introspection>`_.
+"""
+
+SYSVAR_SLOT_HASHES_PUBKEY: PublicKey = PublicKey("SysvarS1otHashes111111111111111111111111111")
+"""The SlotHashes sysvar contains the most recent hashes of the slot's parent banks. It is updated every slot."""
