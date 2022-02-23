d13987c85950b1c6b454c7d4940aa69cb778ea83
Kevin Heavey <kevinheavey123@gmail.com>
2022-02-23 16:52:03 +0000

    migrate blockhash.py to google docstrings

--- src/solana/blockhash.py
+++ src/solana/blockhash.py
@@ -1,8 +1,10 @@
 """Blockhash.
 
->>> # An arbitrary base58 encoded blockhash:
->>> Blockhash("EETubP5AKHgjPAhzPAFcb8BAY1hMH639CWCFTqi3hq1k")
-'EETubP5AKHgjPAhzPAFcb8BAY1hMH639CWCFTqi3hq1k'
+Example:
+
+    >>> # An arbitrary base58 encoded blockhash:
+    >>> Blockhash("EETubP5AKHgjPAhzPAFcb8BAY1hMH639CWCFTqi3hq1k")
+    'EETubP5AKHgjPAhzPAFcb8BAY1hMH639CWCFTqi3hq1k'
 """
 from typing import NewType
 
@@ -15,7 +17,8 @@ Blockhash = NewType("Blockhash", str)
 class BlockhashCache:
     """A recent blockhash cache that expires after a given number of seconds.
 
-    :param ttl: Seconds until cached blockhash expires.
+    Args:
+        ttl: Seconds until cached blockhash expires.
     """
 
     def __init__(self, ttl: int = 60) -> None:
@@ -27,9 +30,11 @@ class BlockhashCache:
     def set(self, blockhash: Blockhash, slot: int, used_immediately: bool = False) -> None:
         """Update the cache.
 
-        :param blockhash: new Blockhash value.
-        :param slot: the slot which the blockhash came from.
-        :param used_immediately: whether the client used the blockhash immediately after fetching it.
+        Args:
+
+            blockhash: new Blockhash value.
+            slot: the slot which the blockhash came from.
+            used_immediately: whether the client used the blockhash immediately after fetching it.
 
         """
         if used_immediately:
@@ -43,7 +48,7 @@ class BlockhashCache:
     def get(self) -> Blockhash:
         """Get the cached Blockhash. Raises KeyError if cache has expired.
 
-        :return: cached Blockhash.
+        Returns: cached Blockhash.
 
         """
         try:
