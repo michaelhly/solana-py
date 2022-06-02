3d5e2e279186d3e1c02af1ab4935c52bed490661
Kevin Heavey <kevinheavey123@gmail.com>
2022-06-02 11:02:35 +0100

    move performance sample tests to their own file

---
+++ tests/integration/test_recent_performance_samples.py
@@ -0,0 +1,26 @@
+"""These tests live in their own file so that their sleeping doesn't slow down other tests."""
+
+import time
+from pytest import mark, fixture
+from .utils import assert_valid_response
+
+
+@fixture
+def wait_until_ready() -> None:
+    """Sleep for a minute so that performance samples are available."""
+    time.sleep(60)
+
+
+@mark.integration
+@mark.asyncio
+async def test_get_recent_performance_samples_async(test_http_client_async, wait_until_ready):
+    """Test get recent performance samples (async)."""
+    resp = await test_http_client_async.get_recent_performance_samples(4)
+    assert_valid_response(resp)
+
+
+@mark.integration
+def test_get_recent_performance_samples(test_http_client, wait_until_ready):
+    """Test get recent performance samples (synchronous)."""
+    resp = test_http_client.get_recent_performance_samples(4)
+    assert_valid_response(resp)
