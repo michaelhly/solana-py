de95458518fb6e9c8374b6032e2d1ff8188659b1
Kevin Heavey <ID+username@users.noreply.github.com>
2022-10-10 22:09:48 +0100

    remove no longer used helper

--- src/solana/utils/helpers.py
+++ src/solana/utils/helpers.py
@@ -1,15 +1,4 @@
 """Helper functions."""
-
-from base64 import b64decode
-
-from based58 import b58decode
-
-
-def from_uint8_bytes(uint8: bytes) -> int:
-    """Convert from uint8 to python int."""
-    return int.from_bytes(uint8, byteorder="little")
-
-
 def to_uint8_bytes(val: int) -> bytes:
     """Convert an integer to uint8."""
     return val.to_bytes(1, byteorder="little")
