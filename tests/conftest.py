3b4c40722b252243de47e2a976cb052d0faf3c8b
Kevin Heavey <kevinheavey123@gmail.com>
2024-02-12 05:34:21 +0400

    lint

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,13 @@ import time
 from typing import NamedTuple
 
 import pytest
-from solana.rpc.api import Client
-from solana.rpc.async_api import AsyncClient
-from solana.rpc.commitment import Processed
 from solders.hash import Hash as Blockhash
 from solders.keypair import Keypair
 from solders.pubkey import Pubkey
 
+from solana.rpc.api import Client
+from solana.rpc.async_api import AsyncClient
+from solana.rpc.commitment import Processed
 from tests.utils import AIRDROP_AMOUNT, assert_valid_response
 
 
