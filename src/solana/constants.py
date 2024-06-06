7b35f69ef69198360ed364e8b98c0531b269a22a
N3tNinja <24782204+n3tn1nja@users.noreply.github.com>
2024-06-06 03:49:22 -0400

    Create solana/constants.py.
    Useful Solana constants.

---
+++ src/solana/constants.py
@@ -0,0 +1,7 @@
+"""Solana constants"""
+
+NATIVE_DECIMALS: int = 9
+"""Standard number of decimals for SOL and wrapped SOL."""
+
+LAMPORTS_PER_SOL: int = 1_000_000_000
+"""Number of lamports per SOL, where 1 SOL equals 1 billion lamports."""
